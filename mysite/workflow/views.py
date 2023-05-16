475bfb4554b2aa628880dc21ae3fb66a135d40c2
Icet0 <49767388+Icet0@users.noreply.github.com>
2023-05-16 14:14:54 +0200

    button id get

--- mysite/workflow/views.py
+++ mysite/workflow/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def workflow_view(request):
+    
+    if request.method == 'POST':
+        button_id = request.POST.get('button_id')
+        print('button_id', button_id)
     return render(request, 'workflow/workflow_page.html')
