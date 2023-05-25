37c3083cb0cb632b7f2a01d3a10bb30ed0ba8b32
Icet0 <49767388+Icet0@users.noreply.github.com>
2023-05-25 17:02:25 +0200

    debut css dashboard

--- mysite/dashboard/views.py
+++ mysite/dashboard/views.py
@@ -2,19 +2,44 @@ import os
 from django.conf import settings
 from django.shortcuts import render
 from .models import Fichier
+from django.http import JsonResponse
 
 # Create your views here.
 
+def get_enfants_view(request):
+    fichier_id = request.GET.get('fichier_id', None)
+    try:
+        fichier = Fichier.objects.get(id=fichier_id)
+        enfants = fichier.enfants.all().values('id', 'nom', 'est_repertoire')
+        return JsonResponse({'enfants': list(enfants)})
+    except Fichier.DoesNotExist:
+        return JsonResponse({'enfants': []})
+
+
+def creer_fichiers_recursif(dossier, parent, user):
+    files = []
+    for nom in os.listdir(dossier):
+        chemin_complet = os.path.join(dossier, nom)
+        est_repertoire = os.path.isdir(chemin_complet)
+        fichier, _ = Fichier.objects.get_or_create(nom=nom, est_repertoire=est_repertoire, user=user)
+        fichier.parent = parent
+        files.append(fichier)
+        if est_repertoire:
+            enfants = creer_fichiers_recursif(chemin_complet, fichier, user)
+            fichier.enfants.set(enfants)
+        fichier.save()
+
+    return files  
+            
 def dashboard_view(request):
     user = request.user
     path = settings.MEDIA_ROOT+"/uploads/" + str(user.username)
     
-    fichiers = os.listdir(path)
-    files = []
-    for fichier in fichiers:
-        chemin_complet = os.path.join(path, fichier)
-        est_repertoire = os.path.isdir(chemin_complet)
-        files.append(Fichier.objects.get_or_create(nom=fichier, est_repertoire=est_repertoire,user=user))
-    fichiers = Fichier.objects.filter(user=request.user)
+    Fichier.objects.all().delete()
+
+    files = creer_fichiers_recursif(path, None, user)
+    print("files : \n",files)
+    fichiers = Fichier.objects.filter(user=request.user, parent=None)
+    print("fichier : \n",fichiers)
     
     return render(request, 'dashboard/dashboard.html', {"fichiers": fichiers})
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -9,7 +9,7 @@ from loadingData.views import load_csv
 from context.views import context_view
 from architecture.views import architecture_view
 from workflow.views import workflow_view
-from dashboard.views import dashboard_view
+from dashboard.views import dashboard_view,get_enfants_view
 
 from search import views as search_views
 
@@ -26,6 +26,7 @@ urlpatterns = [
     path('loading/architecture/', architecture_view, name='architecture_view'),
     path('workflow/', workflow_view , name='workflow_view'),
     path('dashboard/', dashboard_view , name='dashboard_view'),
+    path('get_enfants_view/', get_enfants_view, name='get_enfants_view'),
     
     path(r'', include('allauth.urls')),
     
