1238f0ac80c3520a9c1649af286ac543b3522686
Icet0 <49767388+Icet0@users.noreply.github.com>
2023-07-06 11:35:07 +0200

    path exist ?

--- mysite/features/models.py
+++ mysite/features/models.py
@@ -1,11 +1,15 @@
+import os
 import re
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 
 
-def upload_to(instance, filename):
-    return settings.MEDIA_ROOT+ '/uploads/' + instance.user.username + '/data' + '/' + re.sub(r"[^\w.-]", "-", instance.addFiles.name)
+def upload_to(instance):
+    path = settings.MEDIA_ROOT+ '/uploads/' + instance.user.username + '/data' + '/' + re.sub(r"[^\w.-]", "-", instance.addFiles.name)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
 
 # Create your models here.
 class FeaturesModel(models.Model):
@@ -20,7 +24,7 @@ class FeaturesModel(models.Model):
         
 class AddFormModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
-    addFiles = models.FileField(max_length=650,upload_to=upload_to) 
+    addFiles = models.FileField(max_length=800,upload_to=upload_to) 
 
     
     def __str__(self):
